032f59a3d21a206e1a5c8ae1579546931c24e22a
Aliya.Gayazova <Aliya.Gayazova@innostage-group.ru>
2022-10-21 01:10:36 +0300

    * Настройки SignalR
    * Методы и свойства Context у хаба
    * Обновлен index.html

--- SignalRWeb/SignalRWeb/Hubs/ChatHub.cs
+++ SignalRWeb/SignalRWeb/Hubs/ChatHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace SignalRWeb.Hubs
@@ -7,7 +9,46 @@ namespace SignalRWeb.Hubs
     {
         public async Task Send(string message)
         {
-            await Clients.All.SendAsync("Send", message);
+            var connetionId = Context.ConnectionId; // уникальный идентификатор подключения в виде строки
+            var connectionAborted = Context.ConnectionAborted; // возвращает объект CancellationToken, который извещает о закрытии подключения
+            var user = Context.User; //возвращает объект ClaimsPrincipal, ассоциированный с текущим пользователем. По сути это аналог свойства HttpContext.User, которое доступно в констроллерах
+            var userIdentifier = Context.UserIdentifier; // возвращает идентификатор пользователя.
+            var items = Context.Items; // возвращает словарь значений, ассоциированных с текущим подключением. Данный словарь позволяет хранить данные для определенного клиента между его запросами
+            var features = Context.Features; //возвращает коллекцию возможностей HTTP, ассоциированных с текущим подключением
+
+            //Context.Abort() - принудительно завершает текущее подключение
+            //Context.GetHttpContext() - возвращает объект HttpContext для текущего подключения или null, если подключение не ассоциировано с запросом HTTP.
+
+            await Clients.All.SendAsync("Receive", message, Context.ConnectionId);
+        }
+
+        // срабатывает при подключении нового клиента
+        public override async Task OnConnectedAsync()
+        {
+            var context = this.Context.GetHttpContext();
+            // получаем кук name
+            if (context.Request.Cookies.ContainsKey("name"))
+            {
+                string userName;
+                if (context.Request.Cookies.TryGetValue("name", out userName))
+                {
+                    Debug.WriteLine($"name = {userName}");
+                }
+            }
+            // получаем юзер-агент
+            Debug.WriteLine($"UserAgent = {context.Request.Headers["User-Agent"]}");
+            // получаем ip
+            Debug.WriteLine($"RemoteIpAddress = {context.Connection.RemoteIpAddress.ToString()}");
+
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} вошел в чат");
+            await base.OnConnectedAsync();
+        }
+
+        // срабатывает при отключении клиента, в качестве параметра передается сообщение об ошибке, которая описывает, почему произошло отключение.
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} покинул в чат");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
--- SignalRWeb/SignalRWeb/Program.cs
+++ SignalRWeb/SignalRWeb/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.Extensions.DependencyInjection;
 using SignalRWeb.Hubs;
 
@@ -9,7 +10,20 @@ namespace SignalRWeb
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddSignalR();
+
+            // ѕример глобальной настройки SignalR:
+            builder.Services.AddSignalR(act =>
+            {
+                act.ClientTimeoutInterval = new System.TimeSpan(0, 0, 30); // врем€, в течение которого клиент должен отправить серверу сообщение.  по умолчанию 30 сек
+                act.HandshakeTimeout = new System.TimeSpan(0, 0, 15); //допустимое врем€ таймаута, которое может пройти до получени€ от клиента первого сообщени€ об установки соединени€
+                act.KeepAliveInterval = new System.TimeSpan(0, 0, 15); //если в течение этого периода сервер не отправит никаких сообшений, то автоматически отправл€етс€ ping-сообщение дл€ поддержани€ подключени€ открытым.
+                //act.SupportedProtocols = ; - определ€ет поддерживаемые протоколы. ѕо умолчанию поддерживаютс€ все протоколы.
+                act.EnableDetailedErrors = true; //при значении true возвращает клиенту детальное описание возникшей ошибки
+            })
+                .AddHubOptions<ChatHub>(opt => // пример настройки только дл€ хаба ChatHub
+                {
+                    opt.EnableDetailedErrors = true;
+                });
 
             var app = builder.Build();
             app.MapGet("/", () => "Hello World!");
@@ -19,7 +33,15 @@ namespace SignalRWeb
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHub<ChatHub>("/chat");
+                endpoints.MapHub<ChatHub>("/chat", opt =>
+                {
+                    opt.ApplicationMaxBufferSize = 64; // максимальный размер буфера в байтах, в который сервер помещает получаемые от клиента данные.
+                    // opt.AuthorizationData; представл€ет список (объект IList) объектов IAuthorizeData, которые определ€ют, авторизован ли клиент дл€ подключени€ к хабу.
+                    opt.TransportMaxBufferSize = 64; // максимальный размер буфера в байтах, в который сервер помещает данные дл€ отправки клиенту.
+                    opt.Transports = HttpTransportType.LongPolling | HttpTransportType.ServerSentEvents | HttpTransportType.WebSockets; //представл€ет битовую маску из значений перечислени€ HttpTransportType, котора€ устанавливает допустимые типы транспорта.
+                    opt.LongPolling.PollTimeout = new System.TimeSpan(0, 1, 0); //настраивает транспорт LongPolling.
+                    //opt.WebSockets.CloseTimeout настраивает транспорт WebSocket.
+                });
             });
 
             app.Run();
